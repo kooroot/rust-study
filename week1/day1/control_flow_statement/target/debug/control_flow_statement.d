d372e1308314791b02962b9743582032631cd85d
kooroot <alswo9087@naver.com>
2025-03-27 17:11:03 +0900

    Update first day

---
+++ week1/day1/control_flow_statement/target/debug/control_flow_statement.d
@@ -0,0 +1,1 @@
+/Users/kooroot/Desktop/dev/rust-dev/week1/day1/control_flow_statement/target/debug/control_flow_statement: /Users/kooroot/Desktop/dev/rust-dev/week1/day1/control_flow_statement/src/main.rs
---
+++ week1/day1/control_flow_statement/target/debug/deps/control_flow_statement-94854045442dd0a2.d
@@ -0,0 +1,5 @@
+/Users/kooroot/Desktop/dev/rust-dev/week1/day1/control_flow_statement/target/debug/deps/control_flow_statement-94854045442dd0a2: src/main.rs
+
+/Users/kooroot/Desktop/dev/rust-dev/week1/day1/control_flow_statement/target/debug/deps/control_flow_statement-94854045442dd0a2.d: src/main.rs
+
+src/main.rs:
